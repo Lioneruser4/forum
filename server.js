1565daf5504c630a0c470b5b06d7c7a72021b939
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-11-18 22:13:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.use(express.static("public"));
+
+let posts = [];
+
+app.post("/post", (req, res) => {
+    const { content } = req.body;
+    posts.push(content);
+    res.status(200).send("Gönderi kaydedildi.");
+});
+
+app.listen(port, () => {
+    console.log(`Server http://localhost:${port} adresinde çalışıyor.`);
+});
